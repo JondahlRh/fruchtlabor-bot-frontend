b42e99b9806626bcf92d26b2b4dee45d57af4c5d
Jondahl Rhenius <jondahlrh@gmail.com>
2024-06-01 16:37:46 +0200

    added basic route pages

---
+++ src/app/beschreibung/teamchannels/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold">Team Channels</h1>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+        maxime fugiat dicta quia modi nisi sint laboriosam accusamus, autem
+        praesentium corporis voluptatem consequatur perferendis voluptatibus a
+        mollitia cupiditate beatae, libero obcaecati commodi vel distinctio
+        ducimus asperiores. Velit placeat reprehenderit expedita doloremque
+        ullam consectetur error nobis officiis provident modi. Exercitationem
+        quo sit odit neque! Reiciendis accusantium obcaecati nesciunt a et velit
+        voluptatibus, esse officia perspiciatis?
+      </p>
+    </div>
+  );
+}
