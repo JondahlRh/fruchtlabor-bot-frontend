e8d2cc6a288cc95e1204e8e85e1e39ce32dfa66b
Jondahl Rhenius <jondahlrh@gmail.com>
2024-06-02 11:59:06 +0200

    added loading page

---
+++ src/app/beschreibung/schwarzebrett/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="m-64 flex flex-col items-center gap-2">
+      <div className="h-12 w-12 animate-spin rounded-full border-b-2 border-t-2 border-neutral-400" />
+    </div>
+  );
+}
